83b85d3b7db57f5c951e79d6f7fea749033ed05e
Thalles Ian <thallesyam@users.=.github.com>
2021-01-05 20:42:32 -0300

    tipando ctx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,14 +3,14 @@ import { ServerStyleSheet } from 'styled-components'
 
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
+          enhanceApp: (App: any) => (props: any) =>
             sheet.collectStyles(<App {...props} />),
         })
 
